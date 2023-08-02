d0b2455607df6deb3780439d72891b6168e1e42b
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-08-02 01:36:23 -0600

    Form Base SASS Complete

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,10 +1,10 @@
 <div class="contenedor">
   <h1>UpTask</h1>
-  <p>¡Crea y Administra tus Proyectos!</p>
+  <p class="tagline">¡Crea y Administra tus Proyectos!</p>
 
   <div class="contenedor-sm">
     <p class="desc-pagina">Iniciar Sesión</p>
-    <form method="POST" action="/" class="form">
+    <form method="POST" action="/" class="formulario">
       <div class="campo">
         <label for="email">Email</label>
         <input
@@ -22,11 +22,12 @@
           type="password"
           placeholder="Tu Contraseña"
         >
+      </div>
 
       <input class="boton" type="submit" value="Iniciar Sesión">
-      </div>
+
     </form>
-    <div class="actions">
+    <div class="acciones">
       <a href="/create">Crear Cuenta</a>
       <a href="/forget">Olvidé mi Contraseña</a>
     </div>
