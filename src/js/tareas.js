0e7eb47c489946f22e9017160e55f0cc665fd642
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-02 20:53:10 -0600

    Dashboard Modal Window SASS

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -24,7 +24,18 @@
     setTimeout(() => {
       const formulario = document.querySelector('.formulario');
       formulario.classList.add('animar');
-    }, 3000);
+    }, 0);
+
+    modal.addEventListener('click', function (e) {
+      e.preventDefault();
+      if (e.target.classList.contains('cerrar-modal')) {
+        const formulario = document.querySelector('.formulario');
+        formulario.classList.add('cerrar');
+        setTimeout(() => {
+          modal.remove();
+        }, 500);
+      }
+    });
 
     document.querySelector('body').appendChild(modal);
   }
