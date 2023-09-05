506a0c80f74de37fa1b0842b08ae62b87c9e3a52
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-04 21:21:43 -0600

    Workin Dashboard Tarea Model

---
+++ models/Tarea.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Model;
+
+use Model\ActiveRecord;
+
+class Proyecto extends ActiveRecord
+{
+  protected static $tabla = 'tareas';
+  protected static $columnasDB = ['id', 'nombre', 'estado', 'proyectoId'];
+
+  public $id;
+  public $nombre;
+  public $estado;
+  public $proyectoId;
+
+  public function __construct($args = [])
+  {
+    $this->id = $args['id'] ?? null;
+    $this->nombre = $args['nombre'] ?? '';
+    $this->estado = $args['estado'] ?? 0;
+    $this->proyectoId = $args['proyectoId'] ?? '';
+  }
+}
