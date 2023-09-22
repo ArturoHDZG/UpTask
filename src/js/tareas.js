6e38dbc5b8494f7622e576c556ae8c8e2f03c008
Arturo Hernandez Garza <arturo_hdzg@hotmail.com>
2023-09-21 22:10:15 -0600

    Working on JSON query

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -186,8 +186,27 @@
     actualizarTarea(tarea);
   }
 
-  function actualizarTarea(tarea) {
-    console.log(tarea);
+  async function actualizarTarea(tarea) {
+    const { estado, id, nombre } = tarea;
+
+    const datos = new FormData();
+    datos.append('id', id);
+    datos.append('nombre', nombre);
+    datos.append('estado', estado);
+    datos.append('proyectoId', obtenerProyecto());
+
+    try {
+      const url = '/api/task/edit';
+
+      const respuesta = await fetch(url, {
+        method: 'POST',
+        body: datos
+      });
+
+      const resultado = await respuesta.json();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   function obtenerProyecto() {
